fa96005af4eada43cc2bb3a2da6c6362418881c8
EricHa314 <45398929+EricHa314@users.noreply.github.com>
2018-12-17 22:14:07 +0400

    Add files via upload

---
+++ programming3/classes/grass.js
@@ -0,0 +1,23 @@
+'use strict';
+var Creature = require("./class.creature.js");
+
+module.exports = class Grass extends Creature{
+    
+    constructor(x, y, index){
+        super(x, y, index);
+    }
+
+    mul() {
+        this.multiply++;
+        this.direction = this.yntrelVandak(0)[Math.floor(Math.random() * this.yntrelVandak(0).length)];//random(this.yntrelVandak(0));
+        if (this.multiply >= this.speed && this.direction) {
+            var newGrass = new Grass(this.direction[0], this.direction[1], this.index);
+            newGrass.parentX = this.x;
+            newGrass.parentY = this.y;
+            grassArr.push(newGrass);
+            matrix[this.direction[1]][this.direction[0]] = this.index;
+            this.multiply = 0;
+        }
+    }
+}
+
